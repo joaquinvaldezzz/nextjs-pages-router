ab27c2c83b206803d5772ee580798cf7985b2d9a
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-11-02 22:28:43 +0800

    style: improve document styling

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,8 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta name="description" content="John Joaquin A. Valdez" />
-      </Head>
-      <body className="min-w-80 antialiased">
+      <Head />
+      <body className="min-w-80 bg-white text-gray-900 antialiased [text-rendering:optimizeLegibility]">
         <Main />
         <NextScript />
       </body>
